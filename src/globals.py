2330613633235a9aac8f6dde94589000d869cbc2
cerdamario13 <cerdamario13@gmail.com>
2025-07-04 15:13:05 -0500

    Add year to year globals

--- src/globals.py
+++ src/globals.py
@@ -13,3 +13,8 @@ pi_ws = pi_wb[pi_sheet_name]
 metro_affordability_sheet_name = 'W-7'
 metro_affordability_cell_range = 'A5:E917'
 metro_affordability_ws = pi_wb[metro_affordability_sheet_name]
+
+# Year over Year change in rent and home value
+yoy_change_sheet_name = 'W-10'
+yoy_change_cell_range = 'A4:O107'
+yoy_change_ws = pi_wb[yoy_change_sheet_name]
