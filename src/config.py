ff4e2538cdd16d4a17c85763f264e646c234a9e9
cerdamario13 <cerdamario13@gmail.com>
2025-11-01 13:09:20 -0500

    remove unused config

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class mongo_config():
-    mongo_user = os.getenv("MONGO_USER", "...")
-    mongo_password = os.getenv("MONGO_PASSWORD", "...")
-    mongo_host = os.getenv("MONGO_HOST", "...")
-    mongo_db_name= os.getenv("MONGO_DB_NAME", "...")
-    mongo_collection = os.getenv("MONGO_COLLECTION", "...")
-    
--- src/model.py
+++ src/model.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from config import mongo_config
 from globals import *
 
 # Read the sheet data
